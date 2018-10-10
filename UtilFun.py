10d1bc0501ba8bc3fd4beb9988fdcdf21ad487d8
randysuen1991 <randysuen1991@gmail.com>
2018-10-10 19:44:47 +0800

    new

--- UtilFun.py
+++ UtilFun.py
@@ -1,7 +1,8 @@
 import numpy as np
 from sklearn.preprocessing import OneHotEncoder
 
-def OneHot(labels):
+
+def onehot(labels):
     enc = OneHotEncoder()
     labels = enc.fit_transform(labels)
     return labels.toarray()
@@ -9,55 +10,54 @@ def OneHot(labels):
 
 
 # shape should be (None,height,width,num_channels)
-def vectors2imgs(vecs,shape):
-#    shape = (shape[0][1],shape[0][2])
-    shape = (shape[1],shape[2])
+def vectors2imgs(vecs, shape):
+    shape = (shape[1], shape[2])
     for i in range(vecs.shape[0]):
-        vec = vecs[i,:]
-        img = np.reshape(vec,newshape=shape,order='C')
-        img = np.expand_dims(img,axis=0)
+        vec = vecs[i, :]
+        img = np.reshape(vec, newshape=shape, order='C')
+        img = np.expand_dims(img, axis=0)
         if i == 0:
             imgs_out = img
         else:
-            imgs_out = np.concatenate((imgs_out,img),axis=0)
-    imgs_out = np.expand_dims(imgs_out,axis=3)
+            imgs_out = np.concatenate((imgs_out, img), axis=0)
+    imgs_out = np.expand_dims(imgs_out, axis=3)
     return imgs_out
 
 
 def imgs2vectors(imgs):
     length = imgs.shape[1] * imgs.shape[2]
     for i in range(imgs.shape[0]):
-        img = imgs[i,:,:,0]
-        vec = np.reshape(img,newshape=(1,length),order='F')
-        if i == 0 :
+        img = imgs[i, :, :, 0]
+        vec = np.reshape(img, newshape=(1, length), order='F')
+        if i == 0:
             vecs_out = vec
         else:
-            vecs_out = np.concatenate((vecs_out,vec),axis=0)
+            vecs_out = np.concatenate((vecs_out, vec), axis=0)
             
     return vecs_out
 
-def split_train_test(X_train,Y_train,num_per_sub):
+
+def split_train_test(x_train, y_train, num_per_sub):
     
-    for i in range(int(max(Y_train))):
-        inds = np.where(Y_train.ravel()==i+1)[0]
+    for i in range(int(max(y_train))):
+        inds = np.where(y_train.ravel() == i+1)[0]
         
-        X_train_group_original = X_train[inds,:]
-        Y_train_group_original = Y_train[inds,:]
-        train_inds = np.random.choice(X_train_group_original.shape[0],size=num_per_sub,replace=False)
-        X_train_group = X_train_group_original[train_inds,:]
-        Y_train_group = Y_train_group_original[train_inds,:]
-        X_test_group = np.delete(X_train_group_original,train_inds,axis=0)
-        Y_test_group = np.delete(Y_train_group_original,train_inds,axis=0)
-        if i == 0 :
-            X_train_out = X_train_group
-            Y_train_out = Y_train_group
-            X_test_out = X_test_group
-            Y_test_out = Y_test_group
-        else :
-            X_train_out = np.concatenate((X_train_out,X_train_group),axis=0)
-            Y_train_out = np.concatenate((Y_train_out,Y_train_group),axis=0)
-            X_test_out = np.concatenate((X_test_out,X_test_group),axis=0)
-            Y_test_out = np.concatenate((Y_test_out,Y_test_group),axis=0)
+        x_train_group_original = x_train[inds, :]
+        y_train_group_original = y_train[inds, :]
+        train_inds = np.random.choice(x_train_group_original.shape[0], size=num_per_sub, replace=False)
+        x_train_group = x_train_group_original[train_inds, :]
+        y_train_group = y_train_group_original[train_inds, :]
+        x_test_group = np.delete(x_train_group_original, train_inds, axis=0)
+        y_test_group = np.delete(y_train_group_original, train_inds, axis=0)
+        if i == 0:
+            x_train_out = x_train_group
+            y_train_out = y_train_group
+            x_test_out = x_test_group
+            y_test_out = y_test_group
+        else:
+            x_train_out = np.concatenate((x_train_out, x_train_group), axis=0)
+            y_train_out = np.concatenate((y_train_out, y_train_group), axis=0)
+            x_test_out = np.concatenate((x_test_out, x_test_group), axis=0)
+            y_test_out = np.concatenate((y_test_out, y_test_group), axis=0)
         
-    return X_train_out, Y_train_out, X_test_out, Y_test_out
-        
+    return x_train_out, y_train_out, x_test_out, y_test_out
